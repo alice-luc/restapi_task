a8b35be6d9a9305ed7802c90682392cf85f1c149
Alice Lews <72791938+alice-luc@users.noreply.github.com>
2021-03-31 03:24:23 +0700

    Update settings.py

--- test_api/test_api/settings.py
+++ test_api/test_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '7s&-4pq%c%ya!wf8%)hikxd70fa$$443w_@(n^5+^uly!hp8hw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
